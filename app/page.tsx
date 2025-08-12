05e2eed5e3f676b32c9c8d703930e48a9c51da79
jpwhited <jpwhited1020@gmail.com>
2025-08-12 16:27:17 -0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: 'system-ui, sans-serif' }}>
+      <h1>StageDive</h1>
+      <p>MVP is up. If you see this, the build worked.</p>
+    </main>
+  );
+}
